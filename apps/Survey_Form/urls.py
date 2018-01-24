29afbdd27e3537e24067983b3148e85e3b8899bb
ruslanvs <ruslanvs@yahoo.com>
2018-01-23 19:31:55 -0800

    Assignment complete

--- apps/Survey_Form/urls.py
+++ apps/Survey_Form/urls.py
@@ -1,5 +1,7 @@
 from django.conf.urls import url
 from . import views
 urlpatterns = [
-    url(r'^', views.index )
+    url(r'^$', views.index ),
+    url(r'^index$', views.index ),
+    url(r'^process$', views.form_process ),
 ]
--- apps/Survey_Form/views.py
+++ apps/Survey_Form/views.py
@@ -1,4 +1,14 @@
 from django.shortcuts import render, HttpResponse, redirect
+from django.contrib import messages
+
 def index( request ):
-    
+    # messages.add_message( request, messages.INFO, 'FLASH long' )
+    if 'counter' not in request.session:
+        request.session['counter'] = 0
+    request.session['counter'] += 1
+    messages.success( request, 'Thanks for submitting this form! You have submitted this form ' + str( request.session['counter'] ) + ' times now' )
     return render( request, "Survey_Form/index.html" )
+
+def form_process( request ):
+    print "SUBMIT"
+    return render( request, "Survey_Form/result.html" )
