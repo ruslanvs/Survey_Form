6732a829e391ccb3c28adcd4541a97499234f029
ruslanvs <ruslanvs@yahoo.com>
2018-01-24 12:10:31 -0800

    Duplicate flash messaging fixed

--- apps/Survey_Form/urls.py
+++ apps/Survey_Form/urls.py
@@ -2,7 +2,7 @@ from django.conf.urls import url
 from . import views
 urlpatterns = [
     url(r'^$', views.index ),
-    url(r'^process$', views.form_process ),
+    url(r'^process$', views.process ),
     url(r'^index$', views.index ),
     url(r'^result$', views.result ),
 ]
--- apps/Survey_Form/views.py
+++ apps/Survey_Form/views.py
@@ -3,19 +3,20 @@ from django.contrib import messages
 
 def index( request ):
     # messages.add_message( request, messages.INFO, 'FLASH long' )
+    print "INDEX"
+    return render( request, "Survey_Form/index.html" )
+
+def process( request ):
+    print "SUBMIT"
     if 'counter' not in request.session:
         request.session['counter'] = 0
     request.session['counter'] += 1
     messages.success( request, 'Thanks for submitting this form! You have submitted this form ' + str( request.session['counter'] ) + ' times now' )
-    return render( request, "Survey_Form/index.html" )
-
-def form_process( request ):
-    print "SUBMIT"
     request.session['name'] = request.POST['name']
     request.session['location'] = request.POST['location']
     request.session['language'] = request.POST['language']
     request.session['comment'] = request.POST['comment']
-    print request.POST
+    # print request.POST
     return redirect( '/survey_form/result' )
 
 def result( request ):
